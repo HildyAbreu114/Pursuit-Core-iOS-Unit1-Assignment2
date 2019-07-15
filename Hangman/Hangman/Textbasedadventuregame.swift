51249e259929a182ef1f92973243f3f686117893
hildyabreu114 <hildyabreu@pursuit.org>
2019-07-14 23:21:35 -0400

    project

---
+++ Hangman/Hangman/Textbasedadventuregame.swift
@@ -0,0 +1,9 @@
+//
+//  Textbasedadventuregame.swift
+//  Hangman
+//
+//  Created by hildy abreu on 7/13/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -1,12 +1,144 @@
-//
-//  main.swift
-//  Hangman
-//
-//  Created by Alex Paul on 10/15/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
-//
 
-import Foundation
 
-print("Hello, World!")
+
+
+print("Are you ready to play Hangman? \"yes\" or \"No\"")
+
+var answerInput = readLine()!
+let words = [String](arrayLiteral: "again", "equal", "healthy", "possible")
+let gameWord = words.randomElement
+var emptyWord = [String]()
+var incorrectLetter = [String]()
+var  strikes = 0
+switch answerInput {
+case "Yes":
+    print("Great!!. Which letter will be your first guess?")
+    emptyWord = [readLine()!]
+
+case "No":
+    print("Maybe next time!")
+    emptyWord = [readLine()!]
+
+    
+default:
+    print ("Yes")
+}
+
+var S1 = """
+    ________
+    |       |
+    |       . .
+    |      .   .
+    |       . .
+    |
+    |
+    |
+    |
+    |
+    |
+___________
+"""
+
+var S2 = """
+________
+|       |
+|       . .
+|      .   .
+|       . .
+|        |
+|        |
+|        |
+|        |
+|        |
+|
+___________
+"""
+
+var S3 = """
+________
+|       |
+|       . .
+|      .   .
+|       . .
+|        |
+|   -----|
+|        |
+|        |
+|        |
+|
+___________
+"""
+var S4 = """
+________
+|       |
+|       . .
+|      .   .
+|       . .
+|        |
+|  ----- | -----
+|        |
+|        |
+|        |
+|
+___________
+"""
+
+var S5 = """
+________
+|       |
+|       . .
+|      .   .
+|       . .
+|        |
+|  ----- | -----
+|        |
+|      /
+|     /
+|
+___________
+"""
+var S6 = """
+________
+|       |
+|       . .
+|      .   .
+|       . .
+|        |
+|  ----- | -----
+|        |
+|      /   \
+|     /     \
+|
+___________
+"""
+switch strikes {
+case 0:
+    print("")
+case 1:
+    print(S1)
+case 2:
+    print(S2)
+case 3:
+    print(S3)
+case 4:
+    print(S4)
+case 5:
+    print(S5)
+case 6:
+    print(S6)
+    print ("End! play again?")
+default:
+    print("")
+}
+
+//for incorrectLetter += strikes
+
+//If strikes == (case 6) do {
+//
+//        print ("End! play again?")
+//}
+
+//If userInput == gameword {
+//
+//}
 
